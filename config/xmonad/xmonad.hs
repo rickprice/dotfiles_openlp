0e15500455c1b0439dbab4defd5878e2c50e7759
Frederick Price <rprice@pricemail.ca>
2024-05-21 10:40:53 -0400

    Was the second one to move the window

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -261,7 +261,6 @@ myManageHook =
         -- , manageZoomHook
         -- , className =? "zoom" --> doSink
 --        , className =? "simple-scan" --> doSink
-        , className =? "soffice" --> doShift "PROJECTION"
         , className =? "Soffice" --> doShift "PROJECTION"
 --        , className =? "Gimp" --> doFloat
 --        , className =? "meteo-qt" --> doFloat
