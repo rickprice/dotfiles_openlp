2b521b45e6c28ddf87198755a8b6a1daafb9c790
Frederick Price <rprice@pricemail.ca>
2024-06-04 17:05:24 -0400

    Try logging this way

--- local/bin/openLPOSCControl.sh
+++ local/bin/openLPOSCControl.sh
@@ -4,4 +4,4 @@ set -e -o pipefail
 
 PIPENV_VENV_IN_PROJECT=1
 PIPENV_PIPFILE=~/OpenLPOSCControl/Pipfile
-exec pipenv run openLPOSCControl 2>&1 >~/OpenLPOSCControl.log
+pipenv run openLPOSCControl 2>&1 >~/OpenLPOSCControl.log
