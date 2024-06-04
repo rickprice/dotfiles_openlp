cc87e284a4102a2e9eb282ad92cdc482a6458d98
Frederick Price <rprice@pricemail.ca>
2024-06-04 17:03:43 -0400

    Try to write out logs all the time

--- local/bin/openLPOSCControl.sh
+++ local/bin/openLPOSCControl.sh
@@ -4,4 +4,4 @@ set -e -o pipefail
 
 PIPENV_VENV_IN_PROJECT=1
 PIPENV_PIPFILE=~/OpenLPOSCControl/Pipfile
-exec pipenv run openLPOSCControl
+exec pipenv run openLPOSCControl 2>&1 >~/OpenLPOSCControl.log
