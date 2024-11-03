6157b8e2fd9d2069d38cc22f75c2b658337a7a63
Frederick Price <rprice@pricemail.ca>
2024-11-03 15:22:10 -0500

    Try again

--- local/bin/resetWindowsAV.sh
+++ local/bin/resetWindowsAV.sh
@@ -7,7 +7,15 @@ wmctrl -r "OBS 30" -t 5
 wmctrl -r "OpenLP" -t 5
 wmctrl -r "TouchOSC" -t 5
 
+# Change desktop
+wmctrl -s 0
+
 # Move them into position
-wmctrl -r "TouchOSC" -t 0
-wmctrl -r "OBS 30" -t 0
-wmctrl -r "OpenLP" -t 0
+# wmctrl -r "TouchOSC" -t 0
+# wmctrl -r "OBS 30" -t 0
+# wmctrl -r "OpenLP" -t 0
+
+# Try another way to move them
+wmctrl -R "TouchOSC"
+wmctrl -R "OBS 30"
+wmctrl -R "OpenLP"
