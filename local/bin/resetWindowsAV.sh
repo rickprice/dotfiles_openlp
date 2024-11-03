6dc080952f5b09912dcc04a348e4ce8c3f7fddd4
Frederick Price <rprice@pricemail.ca>
2024-11-03 03:04:18 -0500

    Try again

--- local/bin/resetWindowsAV.sh
+++ local/bin/resetWindowsAV.sh
@@ -8,6 +8,6 @@ wmctrl -r "OpenLP" -t 5
 wmctrl -r "TouchOSC" -t 5
 
 # Move them into position
-wmctrl -r "OpenLP" -t 0
-wmctrl -r "OBS 30" -t 0
 wmctrl -r "TouchOSC" -t 0
+wmctrl -r "OBS 30" -t 0
+wmctrl -r "OpenLP" -t 0
