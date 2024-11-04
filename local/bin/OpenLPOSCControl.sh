499068395effcd391ee3298ae6c6169ea6b266d1
Frederick Price <rprice@pricemail.ca>
2024-11-03 23:28:54 -0500

    Try to fix a problem

--- local/bin/OpenLPOSCControl.sh
+++ local/bin/OpenLPOSCControl.sh
@@ -3,5 +3,5 @@
 set -e -o pipefail
 
 export PIPENV_VENV_IN_PROJECT=1
-export PIPENV_PIPFILE=~/OpenLPOSCControl/Pipfile
+export PIPENV_PIPFILE=/var/run/OpenLPOSCControl/Pipfile
 exec pipenv run openLPOSCControl
